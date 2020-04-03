cc0c235e27ae8d1edac4a88fea44f706f9066a82
Von'Erik Barren-Tucker <vonerikbarren-tucker@VonEriks-MacBook-Pro.local>
2020-04-03 17:10:22 -0400

    initialization of files with dveloper preferences

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React and Material-UI Sandbox Ready</h1>
+
+    </div>
+  );
+}
+
+export default App;
