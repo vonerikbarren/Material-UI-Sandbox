adc8a19f03dbba0f3bd47c50b7d4d4bf92e88b69
Von'Erik Barren-Tucker <vonerikbarren-tucker@VonEriks-MacBook-Pro.local>
2020-04-06 11:00:17 -0400

    testing of bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
 
+// Building a Form (Below)
+// import Form from './components/Class/Form/Form';
+
+
+import HelloBootstrap from './components/Class/Bootstrap/HelloBootstrap';
+import FirstBootstrap from './components/Class/Bootstrap/FirstBootstrap';
+
+
+
 function App() {
   return (
     <div className="App">
       <h1>React and Material-UI Sandbox Ready</h1>
+
+      <FirstBootstrap />
+      <FirstBootstrap />
+      <FirstBootstrap />
+
+
+
+
 
     </div>
   );
