112bfb3dc15dcf74c9cf505a95c58ee398474204
Von'Erik Barren-Tucker <vonerikbarren-tucker@VonEriks-MacBook-Pro.local>
2020-04-06 21:55:14 -0400

    just another commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,40 @@ import './App.css';
 // Building a Form (Below)
 // import Form from './components/Class/Form/Form';
 
-
+//These are testing of bootstrap-themed components
 import HelloBootstrap from './components/Class/Bootstrap/HelloBootstrap';
 import FirstBootstrap from './components/Class/Bootstrap/FirstBootstrap';
+import Bootstrap1 from './components/Class/Bootstrap/BootStrap1';
+import BTSP12 from './components/Class/Bootstrap/BTSP12';
 
+
+import { ThemeProvider } from '@material-ui/core';
+import Theme from './components/MaterialUI/Theme';
+import Grid from './components/MaterialUI/components/GridOne';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>React and Material-UI Sandbox Ready</h1>
+    <ThemeProvider theme={Theme}>
+      <div className="App">
+        <h1>React and Material-UI Sandbox Ready</h1>
 
-      <FirstBootstrap />
-      <FirstBootstrap />
-      <FirstBootstrap />
-
+        <Grid> </Grid>
 
 
 
 
-    </div>
+
+
+
+
+
+      </div>
+
+
+
+    </ThemeProvider>
+
   );
 }
 
